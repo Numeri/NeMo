c9d2043e5fcd0a998ed09bd5ac973f23cca5a63c
Numeri <kaden.uhlig@lilt.com>
2023-09-26 14:59:45 +0200

    Add epoch counts

--- nemo/collections/common/data/dataset.py
+++ nemo/collections/common/data/dataset.py
@@ -70,6 +70,7 @@ class ConcatDataset(IterableDataset):
         self.sampling_kwargs = {}
         self.sampling_scale = sampling_scale
         self.exhaustive = exhaustive
+        self.dataset_epoch_counts = [0] * len(datasets)
 
         if sampling_technique == 'temperature':
             self.index_generator = ConcatDataset.temperature_generator
@@ -141,7 +142,7 @@ class ConcatDataset(IterableDataset):
 
         n = 0
         ind_gen = self.index_generator(self.datasets, **self.sampling_kwargs)
-        while (not self.exhaustive and n < max_elements) or self.exhaustive:
+        while self.exhaustive or n < max_elements:
             n += 1
             try:
                 ind = next(ind_gen)
@@ -153,10 +154,13 @@ class ConcatDataset(IterableDataset):
                     val = self.datasets[ind][val]
                 yield val
             except StopIteration:
+                self.dataset_epoch_counts[ind] += 1
+
                 if ind in non_exhausted_datasets:
                     non_exhausted_datasets.remove(ind)
                     if len(non_exhausted_datasets) == 0:
                         return
+
                 self.iterables[ind] = self.get_iterable(self.datasets[ind])
                 n -= 1
 
